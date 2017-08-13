5253ebc49e54333d22ff751ffdb6e99526fc069e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-08-13 12:29:22 -0700

    Creage geoSchema in developer.js

--- models/developer.js
+++ models/developer.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Geolocation Schema
+const geoSchema = new Schema({
+  type: { type: String, default: 'Point' },
+  coordinates: { type: [Number], index: '2dsphere' }
+});
+
+// Developer Schema
 const developerSchema = new Schema({
   name: {
     type: String,
@@ -13,7 +20,8 @@ const developerSchema = new Schema({
     default: false
   },
   skills: { type: Array, required: [true, 'Skills required'] },
-  age: Number
+  age: Number,
+  geometry: geoSchema
 });
 
 const Developer = mongoose.model('developer', developerSchema);
